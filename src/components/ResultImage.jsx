76043827936c480a0b84c549fa3856913d7cb763
Woxell <andre@woxell.se>
2026-05-11 22:47:10 +0200

    Added download handling function, pass it to DownloadButton

--- src/components/ResultImage.jsx
+++ src/components/ResultImage.jsx
@@ -2,13 +2,25 @@ import DownloadButton from "./DownloadButton"
 
 function ResultImage({ imageSrc }) {
 
+    const download = () => {
+        if (!imageSrc) {
+            return;
+        }
+
+        const link = document.createElement('a');
+        link.href = imageSrc;
+        link.download = 'pixelated-image.png';
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    };
 
     return (
         <div>
             <div className="image-box">
                 <img src={imageSrc} alt="Pixelated result" />
             </div>
-            <DownloadButton />
+            <DownloadButton onClick={download} />
         </div>
     )
 }
